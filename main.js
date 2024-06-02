4b64ffb705c759a2e058d2470c9a083bc7c6a704
Vinayak Kankanwadi <vinayakkankanwadi@hotmail.com>
2024-06-02 12:57:17 +1000

    min

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true // Enable access to Node.js APIs in renderer process
+    }
+  });
+
+  // Load index.html file
+  mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
